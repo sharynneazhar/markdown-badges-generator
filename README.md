d2608bb58586b4423ed0d06f75daadfb8447da6d
Sharynne Azhar <sharynneazhar@gmail.com>
2023-05-08 22:56:28 -0500

    v1

--- README.md
+++ README.md
@@ -1,46 +1,11 @@
-# Getting Started with Create React App
+# markdown-badges generator
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+With this badges generator, you'll be jazzing up your readme files in no time. Just click some boxes and boom, you've got yourself some fancy badges to show off your skills.
 
-## Available Scripts
+Badges retrieved from [ileriayo/markdown-badges](https://github.com/Ileriayo/markdown-badges/tree/master).
 
-In the project directory, you can run:
+![Screenshot of App](React-App.png)
 
-### `npm start`
+### License
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+This project is licensed under the [MIT License](https://github.com/Spiderpig86/Cirrus/blob/master/LICENSE). See the [LICENSE](LICENSE.md) file for details.
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,169 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useCallback, useEffect, useState} from 'react';
+import {Accordion, Button, Form} from 'react-bootstrap';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 import './App.css';
 
 function App() {
+  const [list, setList]: any = useState([]);
+  const [badges, setBadges]: any = useState({});
+
+  const convertMarkdownToObjects = (markdown: any) => {
+    const lines = markdown.split('\n');
+    const sections = [];
+
+    let currentSection = null;
+    let currentBadges = [];
+
+    for (const line of lines) {
+      if (line.startsWith('###')) {
+        if (currentSection) {
+          sections.push({section: currentSection, badges: currentBadges});
+          currentBadges = [];
+        }
+        currentSection = line.substring(4).trim();
+      } else if (
+        line.startsWith('|') &&
+        !line.startsWith('| ---') &&
+        !line.startsWith('| Name')
+      ) {
+        const columns = line.split('|').map((col: any) => col.trim());
+        if (columns && columns.length >= 4) {
+          const name = columns[1];
+          const link = columns[2].match(/\((.*?)\)/)?.[1];
+          currentBadges.push({name, link});
+        }
+      }
+    }
+
+    if (currentSection) {
+      sections.push({section: currentSection, badges: currentBadges});
+    }
+
+    return sections;
+  };
+
+  const fetchMarkdown = useCallback(() => {
+    const url =
+      'https://raw.githubusercontent.com/Ileriayo/markdown-badges/master/README.md';
+    const headers = new Headers();
+
+    // If authentication is required, add the token to the request headers
+    // headers.append('Authorization', 'Bearer ' + token);
+
+    fetch(url, {headers})
+      .then((response) => response.text())
+      .then((data) => {
+        const startIndex = data.indexOf('# Badges');
+        if (startIndex === -1) {
+          console.error('Could not find the "Badges" header');
+          return;
+        }
+
+        const badgesContent = data.substring(startIndex);
+        const badgesList = convertMarkdownToObjects(badgesContent);
+        setList(badgesList);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetchMarkdown();
+  }, [fetchMarkdown]);
+
+  const handleCheckboxes = (event: any, badge: any) => {
+    if (event.target.checked) {
+      setBadges({
+        ...badges,
+        [badge.name]: badge.link,
+      });
+    } else {
+      const filteredBadges = Object.keys(badges)
+        .filter((key) => key !== badge.name)
+        .reduce((obj: any, key) => {
+          obj[key] = badges[key];
+          return obj;
+        }, {});
+      setBadges(filteredBadges);
+    }
+  };
+
+  const generateMarkdownPreview = () => {
+    if (!badges) return '';
+    const markdownContent = Object.entries(badges)
+      .map(([name, link]) => `[![${name}](${link})](${link})`)
+      .join('\n');
+    return markdownContent;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>markdown-badges generator</h1>
       </header>
+      <main>
+        <Accordion defaultActiveKey='0'>
+          {list.map((item: any, index: string) => {
+            return (
+              <Accordion.Item key={index} eventKey={index}>
+                <Accordion.Header>{item.section}</Accordion.Header>
+                <Accordion.Body>
+                  {item.badges.map((badge: any, index: string) => {
+                    return (
+                      <Form.Check
+                        key={index}
+                        type='checkbox'
+                        label={badge.name}
+                        checked={badges[badge.name]}
+                        onChange={(e) => {
+                          handleCheckboxes(e, badge);
+                        }}
+                      />
+                    );
+                  })}
+                </Accordion.Body>
+              </Accordion.Item>
+            );
+          })}
+        </Accordion>
+      </main>
+      <section>
+        <div className='preview'>
+          <h3>
+            <span>Preview</span>
+            <Button variant='light' size='sm' onClick={() => setBadges({})}>
+              Reset
+            </Button>
+          </h3>
+          <div className='badges'>
+            {Object.keys(badges).map((badgeName) => (
+              <span key={badgeName}>
+                <img
+                  src={`${badges[badgeName]}&style=flat-square`}
+                  alt={badgeName}
+                />
+              </span>
+            ))}
+          </div>
+        </div>
+        <div className='markdown'>
+          <h3>
+            <span>Markdown Code</span>
+            <CopyToClipboard text={generateMarkdownPreview()}>
+              <Button variant='light' size='sm'>
+                Copy Code
+              </Button>
+            </CopyToClipboard>
+          </h3>
+          <pre>
+            <code>{generateMarkdownPreview()}</code>
+          </pre>
+        </div>
+      </section>
+      <footer>
+        Made with ❤️ by{' '}
+        <a href='https://github.com/sharynneazhar' className='link-light'>
+          Sharynne Azhar
+        </a>
+      </footer>
     </div>
   );
 }
